6453b5867b9724fae1ae58817def96573969de43
floor777 <bvfwra@gmail.com>
2024-01-12 20:09:27 -0800

    Add random color functionality
    This is to solve the problem of it being the same color only instead of
    random each time

--- scripts.js
+++ scripts.js
@@ -6,21 +6,22 @@ function initiateGrid(n) {
         div.style.opacity = 1;
         div.addEventListener("mouseenter", (event) => {
             if(div.style.opacity > 0) {
-                div.style.opacity -= 0.1;
+                redAmount = getRandomInt(256);
+                greenAmount = getRandomInt(256);
+                blueAmount = getRandomInt(256);
+                div.style.setProperty('opacity', div.style.opacity -= 0.1);
+                div.style.setProperty('background-color', 'rgb(' + redAmount + ',' + greenAmount + ',' + blueAmount + ')');
             }
-            
-
-        });
-
-        div.addEventListener("mouseleave", (event) => {
-            // div.style.opacity = 1;
-
         });
         gridWrapper.appendChild(div);
-
-
     }
 
 }
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+  
+  
 initiateGrid(16)
 
