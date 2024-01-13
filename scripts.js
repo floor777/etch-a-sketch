bb692d63b958448f017c74c69841fa39e9665196
floor777 <bvfwra@gmail.com>
2024-01-12 19:47:37 -0800

    Add functionality where opacity decreases slightly on each div hover

--- scripts.js
+++ scripts.js
@@ -1,8 +1,21 @@
 function initiateGrid(n) {
     let gridWrapper = document.getElementById("grid-wrapper");
-    for(i = 0; i < n * n; i++){
+    for(i = 0; i < n; i++){
         let div = document.createElement('div');
-        div.style.backgroundColor = "red";
+        div.id = "grid-square";
+        div.style.opacity = 1;
+        div.addEventListener("mouseenter", (event) => {
+            if(div.style.opacity > 0) {
+                div.style.opacity -= 0.1;
+            }
+            
+
+        });
+
+        div.addEventListener("mouseleave", (event) => {
+            // div.style.opacity = 1;
+
+        });
         gridWrapper.appendChild(div);
 
 
