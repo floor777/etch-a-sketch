79f156e05dfc9cce822e7cab7463ab74b33164ff
floor777 <bvfwra@gmail.com>
2024-01-13 00:23:56 -0800

    Make each square scale up or down in size to fit
    The previous problem was that squares would not be at the right scale
    where it would fit in the constant sized container. Now they adjust
    according to size n.

--- scripts.js
+++ scripts.js
@@ -1,27 +1,49 @@
 function initiateGrid(n) {
     let gridWrapper = document.getElementById("grid-wrapper");
-    for(i = 0; i < n; i++){
-        let div = document.createElement('div');
-        div.id = "grid-square";
-        div.style.opacity = 1;
-        div.addEventListener("mouseenter", (event) => {
-            if(div.style.opacity > 0) {
+    
+    for (i = 0; i < n * n; i++){
+        let gridRow = document.createElement('gridSquare');
+        gridRow.id = "grid-row-" + i;
+        gridRow.class ="grid-row"
+
+        let gridSquare = document.createElement('gridSquare');
+        gridSquare.className = "grid-square";
+        gridSquare.style.opacity = 1;
+
+        gridSquare.addEventListener("mouseenter", (event) => {
+            if(gridSquare.style.opacity > 0) {
                 redAmount = getRandomInt(256);
                 greenAmount = getRandomInt(256);
                 blueAmount = getRandomInt(256);
-                div.style.setProperty('opacity', div.style.opacity -= 0.1);
-                div.style.setProperty('background-color', 'rgb(' + redAmount + ',' + greenAmount + ',' + blueAmount + ')');
+                gridSquare.style.setProperty('opacity', gridSquare.style.opacity -= 0.1);
+                gridSquare.style.setProperty('background-color', 'rgb(' + redAmount + ',' + greenAmount + ',' + blueAmount + ')');
             }
         });
-        gridWrapper.appendChild(div);
-    }
+        gridSquare.style.flexBasis = 100 / n + "%";
 
+        gridWrapper.appendChild(gridSquare);
+
+    }
 }
 
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
 }
-  
-  
-initiateGrid(16)
 
+function createNewGrid() {
+    newSize = prompt("What size should it be? (n * n) up to 100?")
+    if(newSize > 100) {
+        newSize = 100;
+        alert("Number was higher than 100. Creating a grid of size 100 x 100");
+    }
+    else if(newSize < 0) {
+        newSize = 4;
+        alert("Number was less than 0. making a default 4 x 4 grid");
+    }
+
+    let gridWrapper = document.getElementById("grid-wrapper");
+    while (gridWrapper.firstChild) {
+        gridWrapper.removeChild(gridWrapper.lastChild);
+      }
+    initiateGrid(newSize);
+}
